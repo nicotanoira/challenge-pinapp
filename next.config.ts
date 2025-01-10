e74324d94a26af4efb77744b0330704ce6d64ac4
nicotanoira <nicotanoira@gmail.com>
2025-01-10 00:16:53 -0300

    Config: app configs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['fakestoreapi.com'],
+  },
+  /* other config options can go here */
 };
 
 export default nextConfig;
